849d84525fd630cebde3ecaf34cd3f06cdb55e13
andressamorschel <andressamorschel@gmail.com>
2022-06-22 08:20:39 -0300

    adding docker file in sales-api and auth-api

--- auth-api/app.js
+++ auth-api/app.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 const env = process.env;
-const PORT = env.PORT || 8080;
+const PORT = env.PORT || 8081;
 
 app.get('/api/status', (req, res) => {
     return res.status(200).json({
